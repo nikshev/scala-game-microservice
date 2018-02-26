37bae115d08c3b4b8f964ac0a8d44871dc80a4c5
nikshev <nikshev81@gmail.com>
2018-02-26 16:13:14 +0200

    Some fixes

--- app/controllers/BetsController.scala
+++ app/controllers/BetsController.scala
@@ -2,7 +2,7 @@ package controllers
 
 import javax.inject._
 
-import models.{Bet}
+import models.{Bet, Wallet}
 import play.api.Logger
 import play.api.libs.json._
 import play.api.mvc._
@@ -23,35 +23,107 @@ import scala.concurrent.{ExecutionContext, Future}
 class BetsController @Inject()(val reactiveMongoApi: ReactiveMongoApi)(implicit exec: ExecutionContext) extends Controller with MongoController with ReactiveMongoComponents {
 
   def betsFuture: Future[JSONCollection] = database.map(_.collection[JSONCollection]("bets"))
+  def walletsFuture: Future[JSONCollection] = database.map(_.collection[JSONCollection]("wallets"))
 
-  def place(id: String, amount: Double) = Action.async {
-    Future.successful(BadRequest("Could not build a city from the json provided. " ))
+  /**
+    * Place bet
+    * @return
+    */
+  def place = Action.async(parse.json) { request =>
+    Json.fromJson[Bet](request.body) match {
+      case JsSuccess(bet, _) =>
+        val walletsList = findWalletById(bet.playerId)
+        walletsList.map { wallets =>
+          val walletFromBase = wallets.headOption.getOrElse(Wallet("",0.0))
+          val currentBalance = walletFromBase.balance - bet.amount
+          if (walletFromBase.id != "" && currentBalance > 0) {
+            updateBalanceById(bet.playerId, currentBalance)
+            for {
+              bets <- betsFuture
+              lastError <- bets.insert(bet)
+            } yield
+              Future.successful(Accepted(Json.obj("currentBalance" -> currentBalance)))
+            Accepted(Json.obj("currentBalance" -> currentBalance))
+          } else
+            BadRequest(Json.obj("error" -> "Can't place this sum!!!"))
+        }
+      case JsError(errors) =>
+        Future.successful(BadRequest("Can't create operation from the json provided. " + Errors.show(errors)))
+    }
   }
 
-  def show(id: String) = Action {
+  /**
+    * Show bets
+    * @param id - player/wallet id
+    * @return
+    */
+  def show(id: String) =  Action.async {
     val bets = findBetsById(id)
     // everything's ok! Let's reply with a JsValue
     bets.map { bet =>
       Ok(Json.toJson(bet))
     }
-    BadRequest("Could not build a city from the json provided. " )
+    Future.successful(BadRequest("Can't find bets for:"+Json.obj("playerId" -> id).toString()))
   }
 
+  /**
+    * Find bet by player/wallet id
+    * @param id
+    * @return
+    */
   def findBetsById(id: String) : Future[List[Bet]] = {
     // let's do our query
     val futureBetsList: Future[List[Bet]] =  betsFuture.flatMap {
-      // find all cities with name `name`
+      // find all bets with player id
       _.find(Json.obj("playerId" -> id)).
       // perform the query and get a cursor of JsObject
       cursor[Bet](ReadPreference.primary).
-      // Coollect the results as a list
+      // Collect the results as a list
       collect[List]()
     }
 
     futureBetsList
   }
 
+  /**
+    * Find wallet by id
+    *
+    * @param id - string UUID
+    * @return
+    */
+  def findWalletById(id: String): Future[List[Wallet]] = {
+    // let's do our query
+    val futureWalletsList: Future[List[Wallet]] = walletsFuture.flatMap {
+      // find wallet with name `id`
+      _.find(Json.obj("id" -> id)).
+        // perform the query and get a cursor of JsObject
+        cursor[Wallet](ReadPreference.primary).
+        // Coollect the results as a list
+        collect[List]()
+    }
 
+    futureWalletsList
+  }
+
+
+  /**
+    * Update balance by id
+    * @param id - wallet id
+    * @param currentBalance - balance for update
+    */
+  def updateBalanceById(id: String, currentBalance: Double): Unit = {
+    val modifier = Json.obj(
+      "$set" -> Json.obj(
+        "balance" -> JsNumber(currentBalance)
+      )
+    )
+    for {
+      collection <- walletsFuture
+      lastError <- collection.update(Json.obj("id" -> id), modifier)
+    } yield {
+      Logger.info(s"Successfully inserted with LastError: $lastError")
+    }
+  }
 
 }
 
---
+++ test/ApplicationSpec.scala
@@ -0,0 +1,45 @@
+import org.scalatestplus.play._
+import play.api.libs.json.Json
+import play.api.test._
+import play.api.test.Helpers._
+
+/**
+  * Application test specification
+  */
+class ApplicationSpec extends PlaySpec with OneAppPerTest {
+
+  "Routes" should {
+    "send 404 on a bad request" in  {
+      route(app, FakeRequest(GET, "/boum")).map(status(_)) mustBe Some(NOT_FOUND)
+    }
+
+  }
+
+  "HelthCheckController" should {
+     "answer random string" in {
+       val healthCheck = route(app, FakeRequest(GET, "/healthcheck")).get
+       status(healthCheck) mustBe OK
+       contentType(healthCheck) mustBe Some("application/json")
+     }
+  }
+
+  "BetsController"  should {
+
+    "Place Bet" in {
+      val placeBet = route(app, FakeRequest(POST, "/bets/place")
+        .withJsonBody(Json.obj("playerId" -> "6ef84294-abda-4be7-964c-e4530e91a235", "gameId"->"1", "amount"->1.5))).get
+      status(placeBet) mustBe ACCEPTED
+      contentAsString(placeBet) must include ("currentBalance")
+    }
+
+
+    "Show Bets" in {
+      val bets = route(app, FakeRequest(GET, "/bets?id=6ef84294-abda-4be7-964c-e4530e91a235")).get
+      println(contentAsString(bets))
+      status(bets) mustBe OK
+      contentType(bets) mustBe Some("application/json")
+    }
+
+  }
+
+}
